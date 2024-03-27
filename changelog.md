1ac9efd54c790ada230bde791bdc73f33749bafa
Hyjaxaru <88588952+Hyjaxaru@users.noreply.github.com>
2024-03-27 19:03:01 +0000

    [V2.0.3] Changelog added

---
+++ changelog.md
@@ -0,0 +1,6 @@
+### [V2.0.3]
+
+- changelog added
+- index.html ".about" changed to global ".split-content"
+- tweaks to animation for index.html (index.js)
+- more...
--- javascript/animation/index.js
+++ javascript/animation/index.js
@@ -10,16 +10,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // register gsap plugins
     gsap.registerPlugin(ScrollTrigger,TextPlugin)
 
-    // gsap timeline
-    var intro = gsap.timeline(); // intro anim timeline
+    // intro animation timeline
+    var intro = gsap.timeline();
 
     // logo effect on load
     gsap.from(".nav-logo", { duration: 1, text: "" });
 
-    // nav link effects on load
-    navLinks.forEach((link, index) => {
-        gsap.from(link, { text: navTextLong[index], duration: 0.25 } , (index/5+0.25));
-    })
 
     // welcome section name effect on load
     intro.from(".title .header-name", { opacity: 0, fontSize: 128, text: "", duration: 2, ease: "power4.inOut" });
@@ -27,23 +23,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     intro.from(".title h2", { opacity: 0, y: 10, text:"" }, "<0.5");
     intro.from(".title img", { opacity: 0, y: 10 }, "<0.25");
-
-    //* Scroll Effects
-    // summary section
-    gsap.from(".guarantee", { scrollTrigger: ".guarantee", duration: 1, y: 100, opacity: 0, ease: "power4.inOut" });
-
-    // about section
-    gsap.from(".about", { scrollTrigger: ".about img", duration: 1, y: 100, opacity: 0, ease: "power4.inOut" })
-    gsap.from(".about p", { scrollTrigger: ".about", duration: 2, text: "", ease: "easeOut"})
 });
 
-//* Nav Links Hover Effects
-navLinks.forEach((link, index) => {
-    link.addEventListener("mouseover", () => {
-        gsap.to(link, { duration: 0.25, text: navTextLong[index] }); 
-    });
 
-    link.addEventListener("mouseout", () => {
-        gsap.to(link, { duration: 0.25, text: navTextShort[index] }); 
+//* Nav Links Hover Effects (only when screen is wide enough)
+let mobile = window.matchMedia('(min-width: 650px)');
+if (mobile.matches) {
+    navLinks.forEach((link, index) => {
+        link.addEventListener("mouseover", () => {
+            gsap.to(link, { duration: 0.25, text: navTextLong[index] }); 
+        });
+
+        link.addEventListener("mouseout", () => {
+            gsap.to(link, { duration: 0.25, text: navTextShort[index] }); 
+        });
     });
-})
+};
+
+
+//* Scroll Effects
+// summary section
+gsap.from(".guarantee", { scrollTrigger: ".guarantee", duration: 1, y: 100, opacity: 0, ease: "power4.inOut" });
+
+// about section
+gsap.from(".about img", { scrollTrigger: ".about", duration: 1, y: 100, opacity: 0, ease: "power4.inOut" });
+gsap.from(".about h3", { scrollTrigger: ".about", duration: 1, text: "" });
+gsap.from(".about p", { scrollTrigger: ".about", duration: 2, text: "", ease: "easeOut"});
+
+// skills section
+gsap.from(".skills p", { scrollTrigger: ".skills", duration: 2, text: "", ease: "easeOut"});
